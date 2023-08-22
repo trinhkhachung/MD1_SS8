c1363eec93e4fecbcf762e2ed97ab0fa28862eea
unknown <trinhkhachung27@gmail.com>
2023-08-22 21:25:48 +0700

    ss8

---
+++ in.js
@@ -0,0 +1,20 @@
+let math= +prompt('Điểm môn toán')
+console.log('Điểm môn toán là',math);
+let sinh= +prompt('Điểm môn sinh')
+console.log('ĐIểm môn sinh là',sinh);
+let ly= +prompt('Điểm môn lý là')
+console.log('Điểm môn lý là',sinh)
+let van= +prompt('Điểm môn văn')
+console.log('Điểm môn văn là ',van);
+let tong = math + sinh + ly + van
+console.log('Điểm tổng',tong)
+document.write('Điểm tổng là:',tong)
+document.write('<br>')
+let tb = tong / 4
+console.log('Điểm trung bình',tb);
+document.write('Điểm trung bình là:',tb)
+// let radius=+prompt('bán kính hình tròn');
+// const PI=3.14;
+// let radius= radius*radius*PI;
+// console.log('diện tích hình tròn');
+// console.log('diện tích hình tròn có bán kình ');
